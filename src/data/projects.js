399fe6ae0dcda96a01e9e2c6262d0e0d011348fd
mrlectus <lilbrown3000@gmail.com>
2025-01-26 15:55:11 +0100

    breakpoint 6

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,46 @@
 export const projects = [
   {
     id: 1,
-    img: "public/assets/recent-causes-img-1.png",
+    img: "/assets/recent-causes-img-1.png",
     title: "Safe Net Ambassador's Program",
     description:
       "The Maina & Kids SafeNet Ambassadors Program is an initiative designed to empower Secondary School Students with knowledge on safe and responsible digital practices and also how to use Tech for good. In 2024, we successfully implemented two cohorts, training over 60 ambassadors. Through their peer-to-peer efforts, these ambassadors reached and trained over 545 students across Dutse, Jigawa State. The program emphasizes mentorship, interactive sessions, and community engagement, equipping Secondary School Students to advocate for online safety and positively impact their communities.",
-    heroImg: "public/assets/safenet-amb-program.png",
-    projectImg: "public/assets/project-1.png",
+    heroImg: "/assets/safenet-amb-program.png",
+    projectImg: "/assets/project-1.png",
     idName: "safenet",
     link: "/projects#Safe-Net-Ambassadors-Program",
   },
   {
     id: 2,
-    img: "public/assets/recent-causes-img-2.png",
+    img: "/assets/recent-causes-img-2.png",
     title: "Almajiri in Tech Initiative",
     description:
       "The Almajiri-in-Tech Initiative is a program aimed at empowering marginalized Almajiri boys with basic digital literacy skills. In 2019, we successfully trained 50 boys, equipping them with foundational computer knowledge. Following their training, the participants were enrolled to work in nearby cybercafés, providing them with practical experience and a pathway to economic empowerment. This initiative bridges the digital divide and fosters inclusion by integrating underserved youth into the tech ecosystem.",
-    heroImg: "public/assets/almajiri-in-tech.png",
-    projectImg: "public/assets/project-2.png",
+    heroImg: "/assets/almajiri-in-tech.png",
+    projectImg: "/assets/project-2.png",
     idName: "almajiri",
     link: "/projects#Almajiri-in-Tech-Initiative",
   },
   {
     id: 3,
-    img: "public/assets/recent-causes-img-3.png",
+    img: "/assets/recent-causes-img-3.png",
     title: "Empower Her Future",
     description:
       "Empower Her Future: Street-to-Class is an initiative designed to enroll 50 girls, ages 13-16, who are currently hawking on the streets, back into school. The program aims to provide educational support, including covering tuition and materials. In addition to schooling, the initiative will offer skill development workshops such as sewing and hairdressing. The project also includes psychosocial support to ease their transition and ensure long-term success. Through this initiative, the girls will gain both an education and practical skills for a brighter future. Community involvement will be encouraged to ensure the program's sustainability.",
     heroImg: "public/assets/empower-her-future.png",
-    projectImg: "public/assets/project-3-a.png",
+    projectImg: "/assets/project-3-a.png",
     idName: "empower",
     link: "/projects#Empower-Her-Future",
   },
   {
     id: 4,
-    img: "public/assets/recent-causes-img-4.png",
+    img: "/assets/recent-causes-img-4.png",
     title: "Health Awareness",
     description:
       "The Health Awareness Program for mothers who work as cleaners at Abubakar Tafawa Balewa University, Bauchi, was part of our broader commitment to supporting families and creating healthier environments for children. By reaching out to over 50 hardworking mothers with health education, gift items, gloves, and face masks, we not only appreciated their dedication to maintaining clean hostels but also contributed to the well-being of their families. Healthier mothers ensure healthier homes, fostering better care for their children, which aligns with our mission to uplift children and their communities.",
-    heroImg: "public/assets/safenet-amb-program.png",
-    projectImg: "public/assets/project-1.png",
+    heroImg: "/assets/safenet-amb-program.png",
+    projectImg: "/assets/project-1.png",
     idName: "awareness",
     link: "/projects#Health-Awareness",
   },
 ];
-
